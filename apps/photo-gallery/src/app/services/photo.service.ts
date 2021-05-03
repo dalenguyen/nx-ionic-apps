19da207f0c3dead4b134b16a506e59d17dc66661
Dale Nguyen <dungnq@itbox4vn.com>
2021-05-02 21:14:45 -0400

    feat(gallery): load saved photos

--- apps/photo-gallery/src/app/services/photo.service.ts
+++ apps/photo-gallery/src/app/services/photo.service.ts
@@ -20,6 +20,7 @@ const { Camera, Filesystem, Storage } = Plugins
 })
 export class PhotoService {
   photos: Photo[] = []
+  private PHOTO_STORATE: string = 'photos'
 
   constructor() {}
 
@@ -36,8 +37,33 @@ export class PhotoService {
       this.photos.unshift(savedImageFile)
 
       console.log(this.photos)
+
+      Storage.set({
+        key: this.PHOTO_STORATE,
+        value: JSON.stringify(this.photos),
+      })
     } catch (error) {
-      console.log(error)
+      console.error(error)
+    }
+  }
+
+  async loadSaved() {
+    try {
+      // Retrieve cached photo array data
+      const photoList = await Storage.get({ key: this.PHOTO_STORATE })
+      this.photos = JSON.parse(photoList.value) || []
+
+      for (const photo of this.photos) {
+        const readFile = await Filesystem.readFile({
+          path: photo.filepath,
+          directory: FilesystemDirectory.Data,
+        })
+
+        // web platform only: Load the photo as base64 data
+        photo.webviewPath = `data:image/jpeg;base64,${readFile.data}`
+      }
+    } catch (error) {
+      console.error(error)
     }
   }
 
@@ -60,7 +86,9 @@ export class PhotoService {
         filepath: fileName,
         webviewPath: cameraPhoto.webPath,
       }
-    } catch (error) {}
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   private async _readAsBase64(cameraPhoto: CameraPhoto) {
--- apps/photo-gallery/src/app/tab2/tab2.page.ts
+++ apps/photo-gallery/src/app/tab2/tab2.page.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
-import { PhotoService } from '../services/photo.service';
+import { Component, OnInit } from '@angular/core'
+import { PhotoService } from '../services/photo.service'
 
 @Component({
   selector: 'apps-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
   constructor(public photoService: PhotoService) {}
 
+  ngOnInit(): void {
+    this.photoService.loadSaved()
+  }
+
   addPhotoToGallery() {
-    this.photoService.addNewToGallery();
+    this.photoService.addNewToGallery()
   }
 }
